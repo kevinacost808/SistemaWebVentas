0b9360805d3452165d33dd936f4b974ea02135ba
kevinacosta <kevinacost808@gmail.com>
2023-11-30 23:42:06 -0500

    debug

--- sistema_web_almacen/target/generated-sources/annotations/Rol/Rol_.java
+++ sistema_web_almacen/target/generated-sources/annotations/Rol/Rol_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.12.v20230209-rNA", date="2023-11-16T12:04:40")
+@Generated(value="EclipseLink-2.7.12.v20230209-rNA", date="2023-11-16T12:14:31")
 @StaticMetamodel(Rol.class)
 public class Rol_ { 
 
